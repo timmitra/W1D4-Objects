5f99309ccaf68058717e422e9ca31befcf616f1d
Tim Mitra <twitter@it-guy.com>
2015-06-10 16:02:04 -0400

    Initial Commit

---
+++ Objects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objects
+//
+//  Created by Tim Mitra on 6/10/15.
+//  Copyright (c) 2015 iT Guy Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
