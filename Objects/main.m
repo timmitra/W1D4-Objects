bd26342dc75c1872ffbcb1e9c0c86d0708402d29
Tim Mitra <twitter@it-guy.com>
2015-06-10 16:05:48 -0400

    main Object

--- Objects/main.m
+++ Objects/main.m
@@ -8,10 +8,49 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
-  }
-    return 0;
+@interface Letter : NSObject;
+
+- (void)read;
+
+@end
+
+@implementation Letter
+
+- (void)read
+{
+  NSLog(@"You read a touching personal message.");
 }
+
+@end
+
+
+@interface Newspaper : NSObject;
+
+- (void)read;
+
+@end
+
+@implementation Newspaper
+
+- (void)read
+{
+  NSLog(@"You read a report of the latest going-ons worldwide.");
+}
+
+
+int main(int argc, const char * argv[])
+{
+  
+  @autoreleasepool {
+    
+    // Create two new objects.
+    Newspaper *todaysPaper = [Newspaper new];
+    Letter *todaysLetter = [Letter new];
+    
+    [todaysLetter read];
+    [todaysPaper read];
+  }
+  return 0;
+}
+
+@end
